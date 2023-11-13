63b23e8e0abd89737af9b12ff6c0fc012685b937
nvedad <150713521+nvedad@users.noreply.github.com>
2023-11-13 14:43:02 -0500

    UI

---
+++ BookStore/Controllers/BooksController.cs
@@ -0,0 +1,61 @@
+﻿using DataServices.DBModels;
+using DataServices.Model;
+using DataServices.UnitofWork;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+
+namespace BookStore.Controllers
+{
+    [ApiController]
+    public class BooksController : ControllerBase
+    {
+        private readonly IUoW _context;
+        public BooksController(IUoW context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        [Route("api/GetAllBooks")]
+        public IList<Title> GetAllBooks()
+        {
+            return _context.GetTitlesRepo.GetTitles();
+
+            
+        }
+
+
+        [HttpPost]
+        [Route("api/submitOrder")]
+        public IActionResult SubmitOrder(string title, int qty)
+        {
+            if (title == null || qty < 1)
+            {
+                return BadRequest("Invalid order data");
+            }
+
+            //_context.SubmitOrderRepo.Info(BookName);
+            _context.SubmitOrderRepo.SubmitOrder(title,qty);
+            return Created("","Order placed successfully");
+        }
+
+        [HttpGet]
+        [Route("api/GetReceipt")]
+        public IActionResult Receipt(string id)
+        {
+            //List<Sale>
+            if (id == null)
+            {
+                return BadRequest("Invalid order ID");
+            }
+            List<Sale> result = _context.GetReceiptRepo.GetReceipt(id);
+            if(result.Count>0)
+            return Ok(result);
+
+            return BadRequest("Receipt not found");
+        }
+
+    }
+}
+
---
+++ BookStore/Program.cs
@@ -0,0 +1,40 @@
+using DataServices.DBModels;
+using DataServices.UnitofWork;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var config = builder.Configuration;
+builder.Services.AddDbContext<PubsContext>(options =>
+{
+    options.UseSqlServer(config.GetConnectionString("Books"));
+});
+
+
+builder.Services.AddScoped<IUoW, UoW>();
+
+var app = builder.Build();
+
+
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
